c44643c692cafed38f7f18ab1db949624f55c83e
BernardoSesiSenai <bernardo_ramires@estudante.sesisenai.org.br>
2023-02-24 02:26:46 -0300

    MOdificações no App.js

---
+++ exercicio5/exerciocio-5/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShoppingList from './Components/ShoppingLIst';
+
+function App() {
+  return (
+    <div>
+      <h1>Lista de Compras</h1>
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
